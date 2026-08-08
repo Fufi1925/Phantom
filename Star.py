e5b50dd1235909db5db94504679ca398a37a3c77
dominicWagler-lgtm <dominicWagler@gmx.de>
2026-08-08 11:41:20 +0200

    Update Star.py

--- Star.py
+++ Star.py
@@ -1,5 +1,11 @@
-from phantom.run_dashboard import *
+import os
+import sys
+
+# Fügt den 'phantom'-Ordner zum Suchpfad von Python hinzu
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "/phantom"))
+
+from run_dashboard import *
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("phantom.run_dashboard:app", host="0.0.0.0", port=8080)
+    uvicorn.run("run_dashboard:app", host="0.0.0.0", port=8080)
